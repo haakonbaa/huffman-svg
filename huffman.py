7784cae3583ebd2d560cabb2b4ce4bf8b5bebc20
Håkon Bårsaune <haakonbaa@gmail.com>
2022-05-31 20:17:00 +0200

    tidy

--- huffman.py
+++ huffman.py
@@ -1,3 +1,6 @@
+"""huffman module for finding huffman codes and generating SVGs documenting
+the taken approach."""
+
 import copy
 import drawSvg as draw
 
@@ -32,6 +35,8 @@ class Node:
         return self.value == rhs.value
 
 
+# Takes a dict with characters and corresponding frequencies, creating a tree
+# represented by a list of Nodes
 def huffman(values: dict[str, int]) -> list[Node]:
     values = [Node(ch, val) for ch, val in values.items()]
     while len(values) > 2:
@@ -41,12 +46,15 @@ def huffman(values: dict[str, int]) -> list[Node]:
     return values
 
 
-def traverseTree(tree: list[Node], code: str = '') -> None:
+# Traverses a tree represented by lists of nodes and returns the code for each
+# character in the tree
+def traverseTree(tree: list[Node], code: str = '', result={}) -> dict:
     for i, n in enumerate(tree):
         if n.isBottomNode:
-            print(n.subNodes, f'{code}{i}')
+            result[n.subNodes] = f'{code}{i}'
         else:
-            traverseTree(n.subNodes, f'{code}{i}')
+            traverseTree(n.subNodes, f'{code}{i}', result)
+    return result
 
 
 padx = 15
@@ -56,7 +64,7 @@ diam = 2*radius
 radius_2 = radius/2
 fsize = 12
 
-
+# returns depth of tree represented by list of Nodes
 def _depthOfTree(tree: list[Node]) -> int:
     maxdepth = 0
     for n in tree:
@@ -66,9 +74,7 @@ def _depthOfTree(tree: list[Node]) -> int:
             maxdepth = max(1+_depthOfTree(n.subNodes), maxdepth)
     return maxdepth
 
-# returns number of nodes drawn
-
-
+# Draws a tree on given coordinates
 def _drawTree(tree: list[Node], x: int, y: int, drawing: draw.Drawing, depth: int = 0):
     nodesDrawn = 0
     for i, n in enumerate(tree):
@@ -85,14 +91,16 @@ def _drawTree(tree: list[Node], x: int, y: int, drawing: draw.Drawing, depth: in
                        stroke='black', stroke_width=2, fill='none'))
         drawing.append(draw.Circle(_stopx, _stopy, radius,
                        fill='white', stroke='black', stroke_width=2))
-        drawing.append(draw.Text(str(n.value), fsize, (_startx+_stopx)/2+padx/4, (_starty+_stopy)/2-pady/4))
+        drawing.append(draw.Text(str(n.value), fsize,
+                       (_startx+_stopx)/2+padx/4, (_starty+_stopy)/2-pady/4))
         if n.isBottomNode:
             drawing.append(draw.Text(n.subNodes, fsize, x+(padx+diam)
                            * nodesDrawn, y-pady-diam-fsize/2, center=True, valign=True))
             nodesDrawn += 1
     return nodesDrawn
 
-
+# Takes a dict with characters and corresponding frequencies creating an svg
+# representing how to make a huffman-code from it.
 def huffmanSVG(values: dict[str, int]) -> None:
     numNodes = len(values)
     width = 2*padx+diam+(diam+padx)*(numNodes-1)
@@ -114,31 +122,36 @@ def huffmanSVG(values: dict[str, int]) -> None:
         offsetY += (_depthOfTree(values)+1)*(diam+pady)
     values.sort(reverse=True, key=lambda e: e.value)
     drawing.append(draw.Line(padx, height-pady-offsetY, width-padx, height-pady-offsetY,
-                       stroke='black', stroke_width=2, fill='none'))
+                             stroke='black', stroke_width=2, fill='none'))
     _drawTree(values, padx+radius, height-pady-offsetY, drawing)
     drawing.height = 2*pady+offsetY
     drawing.viewBox = (0, height-offsetY) + (width, drawing.height)
     drawing.viewBox = (drawing.viewBox[0], -drawing.viewBox[1]-drawing.viewBox[3],
-                        drawing.viewBox[2], drawing.viewBox[3])
+                       drawing.viewBox[2], drawing.viewBox[3])
     # _drawTree(values,padx,pady+radius_2,drawing,0)
     drawing.setPixelScale(2)
     drawing.saveSvg('test.svg')
 
 
-letters = {
-    'a': 5,
-    'e': 4,
-    'm': 3,
-    'y': 1,
-    'n': 2,
-    'i': 4,
-    's': 3,
-    'h': 1,
-    'f': 1,
-    'z': 1,
-    'o': 2,
-    't': 3,
-    'r': 4
-}
-traverseTree(huffman(letters))
-huffmanSVG(letters)
+def main():
+    letters = {
+        'a': 5,
+        'e': 4,
+        'm': 3,
+        'y': 1,
+        'n': 2,
+        'i': 4,
+        's': 3,
+        'h': 1,
+        'f': 1,
+        'z': 1,
+        'o': 2,
+        't': 3,
+        'r': 4
+    }
+    print(traverseTree(huffman(letters)))
+    huffmanSVG(letters)
+
+
+if __name__ == '__main__':
+    main()
