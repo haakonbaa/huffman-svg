c8ddf656a21b3a90a9edc49eb2efd9b68940761a
Håkon Bårsaune <haakonbaa@gmail.com>
2022-06-02 20:27:44 +0200

    refactor

--- src/huffmansvg/huffmansvg.py
+++ src/huffmansvg/huffmansvg.py
@@ -1,29 +1,13 @@
 """huffman module for finding huffman codes and generating SVGs documenting
 the taken approach."""
 
-import drawSvg as draw
+import drawSvg
 import math
-
-class Symbols():
-    def __init__(self, symbols : dict):
-        self.symbols = symbols
-        self._sumFreq = sum(symbols.values())
-    
-    def I(self, symbol : str):
-        if symbol not in self.symbols:
-            return 0
-        return math.log2(self._sumFreq/self.symbols[symbol])
-
-    def H(self):
-        return sum(freq/self._sumFreq*self.I(s) for s,freq in self.symbols.items())
-
-    def __repr__(self):
-        return f'Symbols({self.symbols})'
 
 
 class Node:
-    # n1 and n2 are respectively Node and Node
-    # or character and value pair
+    """class representing a node in a huffman tree"""
+
     def __init__(self, n1, n2):
         if isinstance(n1, Node) and isinstance(n2, Node):
             self.value = n1.value + n2.value
@@ -51,9 +35,39 @@ class Node:
         return self.value == rhs.value
 
 
-# Takes a dict with characters and corresponding frequencies, creating a tree
-# represented by a list of Nodes
-def huffman(values: dict[str, int]) -> list[Node]:
+class Symbols():
+    """Class representing a set of symbols and their corresponding whole-number frequencies"""
+
+    def __init__(self, symbols: dict):
+        self.symbols = {str(key): int(value)
+                        for key, value in symbols.items()}
+        self._sumFreq = sum(symbols.values())
+
+    def information(self, symbol: str) -> float:
+        """returns the self-information of a symbol"""
+        if symbol not in self.symbols:
+            return 0.0
+        return math.log2(self._sumFreq/self.symbols[symbol])
+
+    def entropy(self) -> float:
+        """returns the entropy of the set of symbols"""
+        return sum(freq/self._sumFreq*self.information(s) for s, freq in self.symbols.items())
+
+    def huffmanCodes(self) -> dict:
+        """returns a dict of huffman codes generated from the set"""
+        tree = _huffman(self.symbols)
+        return _huffmanCodesTree(tree)
+
+    def encodeSVG(self, filename: str, skip=3):
+        """generate an svg representing how to find the huffman codes. skip is how many steps to skip each frame"""
+        _huffmanSVG(self.symbols, filename, skip)
+
+    def __repr__(self):
+        return f'Symbols({self.symbols})'
+
+
+def _huffman(values: dict) -> list[Node]:
+    """generates a huffman tree represented by a list of Nodes from a dict"""
     values = [Node(ch, val) for ch, val in values.items()]
     while len(values) > 2:
         values.sort(reverse=True, key=lambda e: e.value)
@@ -62,14 +76,13 @@ def huffman(values: dict[str, int]) -> list[Node]:
     return values
 
 
-# Traverses a tree represented by lists of nodes and returns the code for each
-# character in the tree
-def traverseTree(tree: list[Node], code: str = '', result={}) -> dict:
+def _huffmanCodesTree(tree: list[Node], code: str = '', result={}) -> dict:
+    """returns the huffman code for each character in in tree represented by list of Nodes"""
     for i, n in enumerate(tree):
         if n.isBottomNode:
             result[n.subNodes] = f'{code}{i}'
         else:
-            traverseTree(n.subNodes, f'{code}{i}', result)
+            _huffmanCodesTree(n.subNodes, f'{code}{i}', result)
     return result
 
 
@@ -80,8 +93,9 @@ diam = 2*radius
 radius_2 = radius/2
 fsize = 12
 
-# returns depth of tree represented by list of Nodes
+
 def _depthOfTree(tree: list[Node]) -> int:
+    """returns the depth of a tree represented by a list of Nodes"""
     maxdepth = 0
     for n in tree:
         if n.isBottomNode:
@@ -91,7 +105,10 @@ def _depthOfTree(tree: list[Node]) -> int:
     return maxdepth
 
 # Draws a tree on given coordinates
-def _drawTree(tree: list[Node], x: int, y: int, drawing: draw.Drawing, depth: int = 0):
+
+
+def _drawTree(tree: list[Node], x: int, y: int, drawing: drawSvg.Drawing, depth: int = 0):
+    """draws a huffman tree from a given list of Nodes """
     nodesDrawn = 0
     for i, n in enumerate(tree):
         # calculate position of this node
@@ -103,56 +120,53 @@ def _drawTree(tree: list[Node], x: int, y: int, drawing: draw.Drawing, depth: in
         if not n.isBottomNode:
             nodesDrawn += _drawTree(n.subNodes, x+(padx+diam)
                                     * nodesDrawn, y-pady-diam, drawing, depth + 1)
-        drawing.append(draw.Line(_startx, _starty, _stopx, _stopy,
+        drawing.append(drawSvg.Line(_startx, _starty, _stopx, _stopy,
                        stroke='black', stroke_width=2, fill='none'))
-        drawing.append(draw.Circle(_stopx, _stopy, radius,
+        drawing.append(drawSvg.Circle(_stopx, _stopy, radius,
                        fill='white', stroke='black', stroke_width=2))
-        drawing.append(draw.Text(str(n.value), fsize,
+        drawing.append(drawSvg.Text(str(n.value), fsize,
                        (_startx+_stopx)/2+padx/4, (_starty+_stopy)/2-pady/4))
         if n.isBottomNode:
-            drawing.append(draw.Text(n.subNodes, fsize, x+(padx+diam)
+            drawing.append(drawSvg.Text(n.subNodes, fsize, x+(padx+diam)
                            * nodesDrawn, y-pady-diam-fsize/2, center=True, valign=True))
             nodesDrawn += 1
     return nodesDrawn
 
-# Takes a dict with characters and corresponding frequencies creating an svg
-# representing how to make a huffman-code from it.
-def huffmanSVG(values: dict[str, int]) -> None:
+
+def _huffmanSVG(values: dict[str, int], filename: str, skip) -> None:
+    """generates an .svg representing the how to find the huffman tree of a set"""
     numNodes = len(values)
     width = 2*padx+diam+(diam+padx)*(numNodes-1)
     height = 500
     values = [Node(ch, val) for ch, val in values.items()]
-    drawing = draw.Drawing(width, height, displayInline=False)
+    drawing = drawSvg.Drawing(width, height, displayInline=False)
     offsetY = 0
     iters = -1
 
     values.sort(reverse=True, key=lambda e: e.value)
     while len(values) > 2:
         iters += 1
-        if iters % 2 != 0:
+        if iters % skip != 0:
             continue
         values = values[:-2] + [Node(values[-2], values[-1])]
         values.sort(reverse=True, key=lambda e: e.value)
-        drawing.append(draw.Line(padx, height-pady-offsetY, width-padx, height-pady-offsetY,
+        drawing.append(drawSvg.Line(padx, height-pady-offsetY, width-padx, height-pady-offsetY,
                        stroke='black', stroke_width=2, fill='none'))
         _drawTree(values, padx+radius, height-pady-offsetY, drawing)
         offsetY += (_depthOfTree(values)+1)*(diam+pady)
-    # values.sort(reverse=True, key=lambda e: e.value)
-    # drawing.append(draw.Line(padx, height-pady-offsetY, width-padx, height-pady-offsetY,
-    #                          stroke='black', stroke_width=2, fill='none'))
-    # _drawTree(values, padx+radius, height-pady-offsetY, drawing)
     drawing.height = 2*pady+offsetY
     drawing.viewBox = (0, height-offsetY) + (width, drawing.height)
     drawing.viewBox = (drawing.viewBox[0], -drawing.viewBox[1]-drawing.viewBox[3],
                        drawing.viewBox[2], drawing.viewBox[3])
     drawing.setPixelScale(2)
-    drawing.saveSvg('huffman.svg')
+    drawing.saveSvg(filename)
 
-def mdTableHuffman(tree: list[Node]) -> str:
+
+def _mdTableHuffman(tree: list[Node]) -> str:
     result = '|char|code|\n|-|-|\n'
-    result += '\n'.join(f'|{c}|{v}|' for c,v in traverseTree(tree).items())
+    result += '\n'.join(f'|{c}|{v}|' for c,
+                        v in _huffmanCodesTree(tree).items())
     print(result)
-
 
 
 def main():
@@ -172,11 +186,10 @@ def main():
         'r': 4
     }
     a = Symbols(letters)
-    print(a.H()) 
-
-    print(traverseTree(huffman(letters)))
-    huffmanSVG(letters)
-    mdTableHuffman(huffman(letters))
+    print(a.information('o'))
+    print(a.entropy())
+    print(a.huffmanCodes())
+    a.encodeSVG('huffmanTree.svg')
 
 
 if __name__ == '__main__':
